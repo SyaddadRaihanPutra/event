d971653c696f4a3713c4b85d20d813d502af4bd9
Syaddad Raihan Putra <putrasyaddad@gmail.com>
2022-10-31 14:30:23 +0700

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#c3bifb",
+
+          secondary: "#f0abfc",
+
+          accent: "#37CDBE",
+
+          neutral: "#3D4451",
+
+          "base-100": "#c3bifb",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+  //...
+};
